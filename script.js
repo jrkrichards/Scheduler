ada67a9fedeb35404ab430fa77f92570d4a53134
Jayrich <jrkrichards@gmail.com>
2020-10-30 04:43:07 -0700

    built jQuery click event to save appointments

--- script.js
+++ script.js
@@ -19,12 +19,26 @@ const calElementIdsObj = [
     {hour: document.querySelector('#fiveHour'),
     input: document.querySelector('#fiveInput')},
 ]
+const buttonObj = {
+    nineButton: 0,
+    tenButton: 1,
+    elevenButton: 2,
+    twelveButton: 3,
+    oneButton: 4,
+    twoButton: 5,
+    threeButton: 6,
+    fourButton: 7,
+    fiveButton: 8,
+}
 
 // Variables for scripts
 let nowDay = moment().format("dddd, MM/DD/YYYY"); 
 let nowObject = moment().toObject();
 const calTimeObj = [{}]
-const calInputObj = [{}]
+const calInputObj = []
+let appArray = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+const appointments = localStorage.getItem('appointments')
+
 
 // Adjusting HTML
 $("#currentDay").text(nowDay)
@@ -51,9 +65,20 @@ for (let i = 0; i < calElementIdsObj.length; i++) {
 
     
 // EventListeners
-// Ended here need to finish function so input is saved to memory and is appended to the correct input. Do this by saving it to the same object number in the object above then calling that objet number. 
+// Ended here need to finish function so input is saved to memory and is appended to the correct input. Do this by saving it to the same object number in the object above then calling that objet number. consider making an object in here with the key being the id of the button and the value being the index needed to be adjusted. 
 $(":button").click(function (event) {
-    console.log(event.target.id + ' is clicked!');
+    event.preventDefault();
+    let currentId = $(this).attr("id");
+    let currentIndex = buttonObj[currentId] 
+    let currentAppoint = calElementIdsObj[currentIndex].input.value
+    console.log(currentId + ' is clicked!');
+    console.log(currentIndex);
+    console.log(currentAppoint)
+    appArray[currentIndex] = currentAppoint;
+    console.log(appArray)
 
+    localStorage.setItem('appointments', appArray);
+
+   console.log(localStorage.getItem('appointments'));
 });
-
+console.log(appointments)
